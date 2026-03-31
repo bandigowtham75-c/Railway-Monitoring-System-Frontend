def9ae8ac86b3cc9feecd874d2c9d4be304d27f8
bandigowtham75-c <bandigowtham75@gmail.com>
2026-03-31 15:56:35 +0530

    final

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+
 const API = axios.create({
-  baseURL: "http://localhost:8080/api"
+  baseURL:"https://your-backend-url.onrender.com/api",
 });
 
 export default API;
